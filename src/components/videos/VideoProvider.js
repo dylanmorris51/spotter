e962721bc1fd7d30b38498bdbf5d175331cb4a31
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-16 14:18:22 -0500

    added get video by id, edit, and delete, and add fetch calls

--- src/components/videos/VideoProvider.js
+++ src/components/videos/VideoProvider.js
@@ -15,5 +15,42 @@ export const VideoProvider = (props) => {
             .then(res => res.json)
             .then(setVideos)
     }
-    
+
+    //add
+    const addVideo = videoObj => {
+        return fetch("http://localhost:8088/videos", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(videoObj)
+        })
+        .then(getVideos)
+    }
+
+    //get by ID
+    const getVideoById = (id) => {
+        return fetch(`http://localhost:8088/videos/${id}`)
+            .then(res => res.json())
+    }
+
+    //delete
+    const releaseVideo = videoId => {
+        return fetch (`http://localhost:8088/videos/${videoId}`, {
+            method: "DELETE"
+        })
+        .then(getVideos)
+    }
+
+    const updateVideo = video => {
+        return fetch(`http://localhost:8088/videos/${video.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(video)
+        })
+        .then(getVideos)
+    }
+
 }
