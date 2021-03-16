f3fa8eb2cd88ff9268eec1626debe0344e7b0e85
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-16 10:22:54 -0500

    setting up login and auth

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { Spotter } from './spotter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Spotter.js />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
