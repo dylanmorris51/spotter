0c8f81f6c0a2dfecb9df091304b5cd18e99a2871
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-17 14:54:39 -0500

    changed the button on the login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,6 +2,9 @@ import React, { useState } from "react"
 import { Link, useHistory } from "react-router-dom";
 import { authApi, userStorageKey } from "./authSettings"
 import "./Login.css"
+import Form from 'react-bootstrap/Form'
+import Button from "react-bootstrap/Button"
+
 
 
 export const Login = () => {
@@ -38,36 +41,55 @@ export const Login = () => {
     }
 
     return (
-        <main className="container--login">
-            <dialog className="dialog dialog--auth" open={existDialog}>
-                <div>User does not exist</div>
-                <button className="button--close" onClick={e => setExistDialog(false)}>Close</button>
-            </dialog>
-            <section>
-                <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Nutshell</h1>
-                    <h2>Please sign in</h2>
-                    <fieldset>
-                        <label htmlFor="inputEmail"> Email address </label>
-                        <input type="email"
-                            id="email"
-                            className="form-control"
-                            placeholder="Email address"
-                            required autoFocus
-                            value={loginUser.email}
-                            onChange={handleInputChange} />
-                    </fieldset>
-                    <fieldset>
-                        <button type="submit">
-                            Sign in
-                        </button>
-                    </fieldset>
-                </form>
-            </section>
-            <section className="link--register">
-                <Link to="/register">Register for an account</Link>
-            </section>
-        </main>
+        <>
+            <main className="container--login">
+                <dialog className="dialog dialog--auth" open={existDialog}>
+                    <div>User does not exist</div>
+                    <button className="button--close" onClick={e => setExistDialog(false)}>Close</button>
+                </dialog>
+                <section>
+                    <form className="form--login" onSubmit={handleLogin}>
+                        <h1>Spotter</h1>
+                        <h2>Please sign in</h2>
+                        <fieldset>
+                            <label htmlFor="inputEmail"> Email address </label>
+                            <input type="email"
+                                id="email"
+                                className="form-control"
+                                placeholder="Email address"
+                                required autoFocus
+                                value={loginUser.email}
+                                onChange={handleInputChange} />
+                        </fieldset>
+                        <fieldset>
+                            <Button type="submit">
+                                Sign in
+                            </Button>
+                        </fieldset>
+                    </form>
+                </section>
+                <section className="link--register">
+                    <Link to="/register">
+                        <Button>
+                            Register for an account
+                        </Button>
+                    </Link>
+                </section>
+            </main>
+
+            <Form>
+                <Form.Group controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+            </Form>
+
+
+        </>
     )
 }
 
