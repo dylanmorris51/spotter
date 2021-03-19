401a487b111f5f7bbe78117460984be2f3a212a5
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-19 15:38:58 -0500

    got my dropdown to appear on the details page

--- src/components/videos/VideoDetail.js
+++ src/components/videos/VideoDetail.js
@@ -5,18 +5,44 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import YoutubeEmbed from "./YoutubeEmbed"
+import { WorkoutContext } from "../workouts/WorkoutProvider"
+import { WorkoutVideoContext } from "../workoutVideos/WorkoutVideoProvider"
+import DropdownButton from "react-bootstrap/DropdownButton"
+import Dropdown from "react-bootstrap/Dropdown"
+
+//! Add workoutVideo table entry from a button
+//! select workout to add from dropdown
 
 // Detail page
 export const VideoDetail = () => {
 
-    const { getVideoById } = useContext(VideoContext)
-
-    const [video, setVideo] = useState([])
-    console.log('video: ', video);
-
+    //currentUserId, params, & history
+    const currentUserId = +sessionStorage.getItem("app_user_id")
     const { videoId } = useParams()
     const history = useHistory()
+    
+    
+    //video context & state
+    const { getVideoById } = useContext(VideoContext)
+    const [video, setVideo] = useState([])
 
+
+    
+    // workout context 
+    const { workouts, getWorkoutsByUserId } = useContext(WorkoutContext)
+    
+
+    //workoutVideo context
+    const { addWorkoutVideo } = useContext(WorkoutVideoContext)
+
+
+    // gets workoutId from dropdown select
+    const [workoutId, setWorkoutId] = useState(0)
+
+
+
+
+    // fetch video
     useEffect(() => {
         getVideoById(videoId)
             .then((response) => {
@@ -24,7 +50,31 @@ export const VideoDetail = () => {
             })
     }, [])
 
-    //! Make the exit button navigate back to videos page for now, and then to user workout page
+
+    // fetch workouts
+    useEffect(() => {
+        getWorkoutsByUserId(currentUserId)
+    }, [])
+
+
+
+    //dropdown select handler
+    // handle dropdown option select
+    const handleSelect = (e) => {
+        setWorkoutId(e)
+    }
+
+     // add workoutVideoObj
+    const handleAddVideo = (e) => {
+        const workoutVideoObj = {
+            workoutId: +workoutId,
+            videoId: +videoId
+        }
+
+        console.log("new workoutVideoObj", workoutVideoObj)
+        addWorkoutVideo(workoutVideoObj)
+    }
+    
     return (
         <>
             <Jumbotron fluid>
@@ -36,6 +86,24 @@ export const VideoDetail = () => {
                     }}>
                         Back To Videos
                     </Button>
+                    <DropdownButton
+                    alignRight
+                    title="Select Workout"
+                    id="dropdown-menu-align-right"
+                    onSelect={handleSelect}
+                    >
+                        <Dropdown.Item eventKey="0">Select a workout...</Dropdown.Item>
+                    {
+                        workouts.map(workout => {
+                            
+                            return <Dropdown.Item eventKey={workout.id}>{workout.name}</Dropdown.Item>
+
+                        })
+                    }
+                </DropdownButton>
+
+
+
                 </Container>
             </Jumbotron>
         </>
