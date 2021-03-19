bf69d6f3f3c8a84e705289aa4c14a854e33ffbbd
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-19 09:29:51 -0500

    making a workout form for naming workouts. Need to determine if new exercises will be added via the form, or via a different screen

--- src/components/workouts/WorkoutForm.js
+++ src/components/workouts/WorkoutForm.js
@@ -7,7 +7,7 @@ import { WorkoutContext } from "./WorkoutProvider"
 export const WorkoutForm = () => {
 
     //context
-    const { addWorkout, getWorkoutbyId, updateWorkout } = useContext(WorkoutContext)
+    const { getWorkouts, addWorkout, getWorkoutbyId, updateWorkout } = useContext(WorkoutContext)
 
     //state
     const [workout, setWorkout] = useState({
@@ -18,25 +18,85 @@ export const WorkoutForm = () => {
     //enable save
     const [isLoading, setIsLoading] = useState(true)
 
-    //workoutId
+    //workoutId + navigation
     const { workoutId } = useParams()
     const history = useHistory() 
 
-    // event grabber
+
+    //check for edit or add
+    useEffect(() => {
+        getWorkouts()
+            .then(() => {
+                if (workoutId) {
+                    getWorkoutbyId(workoutId)
+                        .then(res => {
+                            setWorkout(res)
+                            setIsLoading(false)
+                        })
+                } else {setIsLoading(false)}
+            })
+    }, [])
+
+
+
+
+    // input handler
     const handleControlledInputChange = (event) => {
         
         const newWorkout = { ...workout}
 
-        newWorkout[event.target.id] = event.target.value
+        let selectedVal = e.target.value
+        if (e.target.id.includes("Id")) {
+            selectedVal = +selectedVal
+        }
+
+        newWorkout[event.target.id] = selectedVal
 
         setWorkout(newWorkout)
     }
 
-    // save workout
-    const handleSaveWorkout = () => {
-        //TODO: reference kennels to create the rest of this form
+    // save  hndler
+    const handleSaveWorkout = (event) => {
+        
+        setIsLoading(true)
+            
+        const currentUserId = +sessionStorage.getItem("app_user_id")
+
+        if (workoutId) {
+            updateWorkout({
+                name: workout.name,
+                userId: workout.currentUserId,
+                id: workout.id
+            }).then(() => history.push(`/workouts/detail/${workout.id}`))
+        } else {
+            addWorkout({
+                name: workout.name,
+                userId: currentUserId
+            }).then(() => history.push("/workouts"))
+        }
     }
 
+    return (
+        <form className="workoutForm">
+            <h2 className="workoutFormTitle">{workoutId ? "Edit Workout" : "Add Workout"}</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="title">Workout Name: </label>
+                    <input type="name" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="name?" value={workout.name} />
+                </div>
+            </fieldset>
+            <fieldset>
+            </fieldset>
+            <button className="btn btn-primary"
+                disabled={isLoading}
+                onClick={event => {
+                    event.preventDefault()
+                    handleSaveEvent()
+                }}>
+                {eventId ? "Save Workout" : "Add Workout"}
+            </button>
+        </form>
+    )
 
 
 
