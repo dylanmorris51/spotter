ea5112387dc0d884c870109e6432e4974f39218d
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-22 14:51:41 -0500

    Deleting a workout from the workout card list works! Now testing if a cascading delete occurs when I delete a workout that has workoutVideos associated with it in the join table

--- src/components/workouts/WorkoutCard.js
+++ src/components/workouts/WorkoutCard.js
@@ -1,23 +1,38 @@
-import React from "react"
+import React, { useContext } from "react"
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import { useHistory } from "react-router-dom"
 import { Link } from "react-router-dom"
+import { WorkoutContext } from "./WorkoutProvider"
 
 
-export const WorkoutCard = ({ workout }) => (
-    <Card style={{ width: '18rem' }}>
-        <Card.Header>{ workout.name }</Card.Header>
-            
+export const WorkoutCard = ({ workout }) => {
+    
+    const { deleteWorkout } = useContext(WorkoutContext)
+    
+    
+    return  <>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Header>{ workout.name }</Card.Header>
+                        
 
-        <Card.Body>
-            <Button variant="primary" href={`/workouts/detail/${workout.id}`}>
-                View Workout
-            </Button>
-            <Button variant="primary" href={`/workouts/edit/${workout.id}`}>
-                Edit Workout
-            </Button>
-        </Card.Body>
-    </Card>
+                    <Card.Body>
+                        <Button variant="primary" href={`/workouts/detail/${workout.id}`}>
+                            View Workout
+                        </Button>
+                        <Button variant="primary" href={`/workouts/edit/${workout.id}`}>
+                            Edit Workout
+                        </Button>
+                        <Button onClick={() => (
+                            deleteWorkout(workout.id)
+                        )}>
+                            Delete Workout
+                        </Button>
+                        <Button href={`/videos`}>
+                            Add Videos
+                        </Button>
+                    </Card.Body>
+                </Card>
+            </>
 
-)
+}
