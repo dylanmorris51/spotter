0285613fe37dccf691f71413ea2ee2c35d0900b3
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-22 15:43:43 -0500

    added files for list and cards

--- src/components/workouts/WorkoutList.js
+++ src/components/workouts/WorkoutList.js
@@ -1,6 +1,4 @@
-//! Display all workouts for this user
 //! If none, prompt the user to create a workout
-//! If some, display as clickable boxes which navigate to that workout on a details page
 
 import React, { useContext, useEffect, useState } from "react"
 import { useHistory, useParams } from "react-router-dom"
