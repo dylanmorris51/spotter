d72e5d31c2680eb13093800dcd20e1b4b075ac80
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-30 15:46:17 -0500

    added github link and logo with copyright info to the footer

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -9,21 +9,6 @@ export const NavBar = (props) => {
     return (
 
         <>
-            {/*<ul className="navBar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/">Home</Link>
-        </li>
-        <li className="navbar__item">
-                <Link className="navbar__link" to="/videos">Videos</Link>
-            </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/collections">Collections</Link>
-            </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/planner">Planner</Link>
-            </li>
-            
-        </ul> */}
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand href="/">
                     <img
